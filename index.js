5e8c555f76f0ac981450d32f5b255538bee084a9
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-12-06 15:38:59 -0500

    commit tree f5ed9f17d590fc1716f31fa5257ee2a0542568d6

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+  function reverseString(word) {
+    return word.split("").reverse().join("");
+  }
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  return word === reversedWord;
 }
 
 /* 
@@ -15,6 +22,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
